fee190e2b4d43b8a2094df42fbf6ec0d052216a0
Кузьмин Глеб Александрович <gakuzmin@edu.hse.ru>
2025-03-29 00:01:29 +0300

    Implemented player and master forms\nRestructured code

--- bot/dialogs/master_form.py
+++ bot/dialogs/master_form.py
@@ -1,20 +1,200 @@
+from typing import Optional
+
 from aiogram.types import CallbackQuery, ContentType, Message
-from aiogram_dialog import Dialog, Window, DialogManager
+from aiogram_dialog import Dialog, Window, DialogManager, ShowMode
+from aiogram_dialog.widgets.common import Whenable
 from aiogram_dialog.widgets.input import MessageInput
-from aiogram_dialog.widgets.text import Const, Format
+from aiogram_dialog.widgets.text import Const, Format, Multi, List
 from aiogram_dialog.widgets.kbd import Button, Row, Column, Back, SwitchTo, Select, Group, Cancel, Start
 
+from bot.dialogs.general_tools import need_to_display_current_value, go_back_when_edit_mode, raise_keyboard_error, \
+    switch_state, generate_save_user_experience
 from bot.states.general_states import Registration, Profile, PlayerForm, MasterForm
 
-# TODO: change to database query
-from bot.dialogs.profile import user
+from bot.db.current_requests import user, get_user_master
 
-# TODO: implement player form
+
+# SELECTORS
+def is_user_playing_online(data: Optional[dict], widget: Optional[Whenable], manager: Optional[DialogManager]):
+    role = user["general"].get("format")
+    return role == "Онлайн" or role == "Оффлайн и Онлайн"
+
+
+def is_user_playing_offline(data: Optional[dict], widget: Optional[Whenable], manager: Optional[DialogManager]):
+    role = user["general"].get("format")
+    return role == "Оффлайн" or role == "Оффлайн и Онлайн"
+
+
+# Saving player form settings (ONCLICK)
+save_experience_master = generate_save_user_experience("master", MasterForm.choosing_cost)
+
+
+async def save_cost(callback: CallbackQuery, button: Button, manager: DialogManager):
+    next_stages = {"edit": None, "register": None}
+    if button.widget_id == "cost_free":
+        next_stages = {"edit": None, "register": MasterForm.typing_place}
+        user["master"]["cost"] = "Бесплатно"
+    elif button.widget_id == "cost_paid":
+        next_stages = {"edit": MasterForm.typing_cost, "register": MasterForm.typing_cost}
+        user["master"]["cost"] = "Платно"
+    else:
+        await raise_keyboard_error(callback, "цену")
+
+    await switch_state(manager, next_stages)
+
+
+# TODO: generate these types of onclick functions
+async def save_cost_number(message: Message, message_input: MessageInput, manager: DialogManager):
+    user["master"]["cost"] = message.text
+
+    if is_user_playing_offline(None, None, None):
+        next_stages = {"edit": None, "register": MasterForm.typing_place}
+    else:
+        next_stages = {"edit": None, "register": MasterForm.typing_platform}
+    await switch_state(manager, next_stages)
+
+
+async def save_place(message: Message, message_input: MessageInput, manager: DialogManager):
+    user["master"]["place"] = message.text
+
+    if is_user_playing_online(None, None, None):
+        next_stages = {"edit": None, "register": MasterForm.typing_platform}
+    else:
+        next_stages = {"edit": None, "register": MasterForm.typing_requirements}
+    await switch_state(manager, next_stages)
+
+
+async def save_platform(message: Message, message_input: MessageInput, manager: DialogManager):
+    user["master"]["platform"] = message.text
+
+    next_stages = {"edit": None, "register": MasterForm.typing_requirements}
+    await switch_state(manager, next_stages)
+
+
+async def save_requirements(message: Message, message_input: MessageInput, manager: DialogManager):
+    user["master"]["place"] = message.text
+
+    next_stages = {"edit": None, "register": None}
+    await switch_state(manager, next_stages)
+
+
 # Master form
 dialog = Dialog(
+    # Checking profile
     Window(
-        Const("Анкета мастера."),
-        Cancel(Const("Завершить")),
-        state=MasterForm.master_form,
+        Multi(
+            Format(
+                "Это ваша анкета мастера, заполнив её, вы установите значения по умолчанию для ваших игр. Вы сможете использовать их или выбрать другие параметры при создании заявки.\n\n" +
+                "<b>Опыт</b>: {experience}\n" +
+                "<b>Цена</b>: {cost}\n"
+            ),
+            Format(
+                text="<b>Место проведения</b>: {place}\n",
+                when=is_user_playing_offline
+            ),
+            Format(
+                text="<b>Платформа</b>: {platform}\n",
+                when=is_user_playing_online
+
+            ),
+            Format(
+                "<b>Требования к игрокам</b>: {requirements}"
+            ),
+            sep=""
+        ),
+
+        SwitchTo(Const("Редактировать анкету"), state=MasterForm.choosing_what_to_edit, id="edit_form_player",
+                 show_mode=ShowMode.SEND),
+        Cancel(Const("Выйти")),
+
+        parse_mode="HTML",
+        state=MasterForm.checking_info,
     ),
+    # Editing profile
+    Window(
+        Const("Выберите, что хотите отредактировать."),
+
+        Start(Const("Опыт"), state=MasterForm.choosing_experience, id="edit_experience_master", data={"mode": "edit"}),
+        Start(Const("Цена"), state=MasterForm.choosing_cost, id="edit_cost_master", data={"mode": "edit"}),
+        Start(Const("Место проведения"), state=MasterForm.typing_place, id="edit_place_master", data={"mode": "edit"}, when=is_user_playing_offline),
+        Start(Const("Платформа"), state=MasterForm.typing_platform, id="edit_place_master", data={"mode": "edit"}, when=is_user_playing_online),
+        Start(Const("Требования у игрокам"), state=MasterForm.typing_requirements, id="edit_requirements_master",
+              data={"mode": "edit"}),
+        Start(Const("Заполнить анкету заново"), state=MasterForm.choosing_experience, id="register_again_master",
+              data={"mode": "register"}),
+        Back(Const("Назад"), id="back_to_checking_info_master"),
+
+        state=MasterForm.choosing_what_to_edit,
+    ),
+    # Getting experience
+    Window(
+        Const("Каков ваш опыт в НРИ в качестве мастера?"),
+        Format("\n<b>Текущее значение</b>: {experience}", when=need_to_display_current_value),
+
+        Button(Const("Менее 3 месяцев"), id="experience_0_master", on_click=save_experience_master),
+        Button(Const("От 3 месяцев до 1 года"), id="experience_1_master", on_click=save_experience_master),
+        Button(Const("От 1 до 3 лет"), id="experience_2_master", on_click=save_experience_master),
+        Button(Const("От 3 до 10 лет"), id="experience_3_master", on_click=save_experience_master),
+        Button(Const("Более 10 лет"), id="experience_4_master", on_click=save_experience_master),
+
+        go_back_when_edit_mode,
+        parse_mode="HTML",
+        state=MasterForm.choosing_experience,
+    ),
+    Window(
+        Const("Какие игры вы планируете проводить?"),
+        Format("\n<b>Текущее значение</b>: {cost}", when=need_to_display_current_value),
+
+        Button(Const("Бесплатные"), id="cost_free", on_click=save_cost),
+        Button(Const("Платные"), id="cost_paid", on_click=save_cost),
+
+        go_back_when_edit_mode,
+        parse_mode="HTML",
+        state=MasterForm.choosing_cost,
+    ),
+    # Getting cost
+    Window(
+        Const("Сколько вы планируете брать за проведение сессии? Введите ответ в свободной форме"),
+        Format("\n<b>Текущее значение</b>: {cost}", when=need_to_display_current_value),
+
+        MessageInput(func=save_cost_number, content_types=[ContentType.TEXT]),
+
+        go_back_when_edit_mode,
+        parse_mode="HTML",
+        state=MasterForm.typing_cost,
+    ),
+    # Getting place
+    Window(
+        Const("Где вы планируете проводить игры?"),
+        Format("\n<b>Текущее значение</b>: {place}", when=need_to_display_current_value),
+
+        MessageInput(func=save_place, content_types=[ContentType.TEXT]),
+
+        go_back_when_edit_mode,
+        parse_mode="HTML",
+        state=MasterForm.typing_place,
+    ),
+    # Getting platform
+    # TODO: add choice of platform (like roll20 or foundry) and connection way (discord, telegram, etc)
+    Window(
+        Const("Какую платформу вы будете использовать для проведения игр? Укажите её и способ общения во время игры."),
+        Format("\n<b>Текущее значение</b>: {platform}", when=need_to_display_current_value),
+
+        MessageInput(func=save_platform, content_types=[ContentType.TEXT]),
+
+        go_back_when_edit_mode,
+        parse_mode="HTML",
+        state=MasterForm.typing_platform,
+    ),
+    Window(
+        Const("Каким требованиям должны удовлетворять игроки, которых вы ищите?"),
+        Format("\n<b>Текущее значение</b>: {requirements}", when=need_to_display_current_value),
+
+        MessageInput(func=save_requirements, content_types=[ContentType.TEXT]),
+
+        go_back_when_edit_mode,
+        parse_mode="HTML",
+        state=MasterForm.typing_requirements,
+    ),
+    getter=get_user_master,
 )
--- bot/dialogs/profile.py
+++ bot/dialogs/profile.py
@@ -1,46 +1,35 @@
 from aiogram.types import CallbackQuery, ContentType, Message
-from aiogram_dialog import Dialog, Window, DialogManager
+from aiogram_dialog import Dialog, Window, DialogManager, ShowMode
 from aiogram_dialog.widgets.input import MessageInput
-from aiogram_dialog.widgets.text import Const, Format
+from aiogram_dialog.widgets.text import Const, Format, Jinja
 from aiogram_dialog.widgets.kbd import Button, Row, Column, Back, SwitchTo, Select, Group, Cancel, Start
 
 from bot.states.general_states import Registration, Profile, PlayerForm, MasterForm
 
-# TODO: implement database
-user = {
-    "general": {"name": "", "age": "", "city": "", "time_zone": "", "role": "", "format": "", "about_info": ""},
-    "player": {},
-    "master": {}
-}
-
-
-# Passing arguments to dialog (GETTERS)
-# TODO: change to database query
-async def get_user_general(**kwargs):
-    return user["general"]
-
+from bot.db.current_requests import user, get_user_general
 
 # Profile dialog
 dialog = Dialog(
     # Checking profile
     Window(
-        # TODO: access to database
         Format(
-            "Ваш профиль:\n\n" +
-            "Имя: {name}\n" +
-            "Возраст: {age}\n" +
-            "Город: {city}\n" +
-            "Часовой пояс: {time_zone}\n" +
-            "Роль: {role}\n" +
-            "Формат игры: {format}\n" +
-            "О себе: {about_info}.\n",
+            "Это ваш профиль, в нём указана основная информация о вас. \nЕё увидят игроки, если вы опубликуете игру, и мастера, если вы откликнитесь на существующую заявку.  \n\n" +
+            "<b>Имя</b>: {name}\n" +
+            "<b>Возраст</b>: {age}\n" +
+            "<b>Город</b>: {city}\n" +
+            "<b>Часовой пояс</b>: {time_zone}\n" +
+            "<b>Роль</b>: {role}\n" +
+            "<b>Формат игры</b>: {format}\n" +
+            "<b>О себе</b>: {about_info}\n",
         ),
 
-        SwitchTo(Const("Редактировать анкету"), state=Profile.choosing_what_to_edit, id="edit_form"),
-        Row(Start(Const("Анкета игрока"), state=PlayerForm.player_form, id="player_form_from_profile"),
-            Start(Const("Анкета мастера"), state=MasterForm.master_form, id="master_form_from_profile"), ),
+        SwitchTo(Const("Редактировать анкету"), state=Profile.choosing_what_to_edit, id="edit_form",
+                 show_mode=ShowMode.SEND),
+        Row(Start(Const("Анкета игрока"), state=PlayerForm.checking_info, id="player_form_from_profile"),
+            Start(Const("Анкета мастера"), state=MasterForm.checking_info, id="master_form_from_profile"), ),
         Cancel(Const("Выйти")),
 
+        parse_mode="HTML",
         getter=get_user_general,
         state=Profile.checking_info,
     ),
@@ -48,19 +37,19 @@ dialog = Dialog(
     Window(
         Const("Выберите, что хотите отредактировать."),
 
-        Start(Const("Имя"), state=Registration.typing_nickname, id="edit_nickname", data={"mode": "edit"}),
-        Start(Const("Возраст"), state=Registration.typing_age, id="edit_age", data={"mode": "edit"}),
-        Start(Const("Город"), state=Registration.choosing_city, id="edit_city", data={"mode": "edit"}),
+        Start(Const("Имя"), state=Registration.typing_nickname, id="edit_nickname_general", data={"mode": "edit"}),
+        Start(Const("Возраст"), state=Registration.typing_age, id="edit_age_general", data={"mode": "edit"}),
+        Start(Const("Город"), state=Registration.choosing_city, id="edit_city_general", data={"mode": "edit"}),
         # TODO: ? forbid changing time zone than city is default
-        Start(Const("Часовой пояс"), state=Registration.choosing_time_zone, id="edit_time_zone", data={"mode": "edit"}),
-        Start(Const("Роль"), state=Registration.choosing_role, id="edit_role", data={"mode": "edit"}),
-        Start(Const("Формат игры"), state=Registration.choosing_format, id="edit_format", data={"mode": "edit"}),
-        Start(Const("О себе"), state=Registration.typing_about_information, id="edit_about_info",
+        Start(Const("Часовой пояс"), state=Registration.choosing_time_zone, id="edit_time_zone_general", data={"mode": "edit"}),
+        Start(Const("Роль"), state=Registration.choosing_role, id="edit_role_general", data={"mode": "edit"}),
+        Start(Const("Формат игры"), state=Registration.choosing_format, id="edit_format_general", data={"mode": "edit"}),
+        Start(Const("О себе"), state=Registration.typing_about_information, id="edit_about_info_general",
               data={"mode": "edit"}),
-        Start(Const("Пройти регистрацию заново"), state=Registration.typing_nickname, id="register_again",
+        Start(Const("Пройти регистрацию заново"), state=Registration.typing_nickname, id="register_again_general",
               data={"mode": "register"}),
-        Back(Const("Назад"), id="back_to_checking_info"),
+        Back(Const("Назад"), id="back_to_checking_info_general"),
 
         state=Profile.choosing_what_to_edit,
-    )
+    ),
 )
